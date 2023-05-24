f97321ee58d51249526e65b867434cebd75f77d0
Vache95 <vachevirabyan1995@gmail.com>
2023-05-24 18:45:33 +0400

    fix

--- src/page/checkout/payments/index.tsx
+++ src/page/checkout/payments/index.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent, useState } from "react";
+import { FC, FormEvent, useState, useEffect } from "react";
 
 import Form from "react-bootstrap/Form";
 import { useForm } from "react-hook-form";
@@ -12,10 +12,9 @@ import Address from "assets/svg/Vector (2).svg";
 import Org from "assets/svg/Vector (3).svg";
 import Input from "components/formElements/input";
 import { THANK_YOU } from "constant";
-import { CardNumberElement } from "@stripe/react-stripe-js";
 
 import { countries } from "config/config";
-import { useStripe, useElements } from "@stripe/react-stripe-js";
+import { useStripe, useElements, CardNumberElement, PaymentRequestButtonElement } from "@stripe/react-stripe-js";
 import { FormData } from "@types";
 import { useExpensesData } from "context";
 
@@ -24,6 +23,7 @@ import "./payments.scss";
 const Payments: FC = (): JSX.Element => {
   const [option, setOption] = useState<boolean>(false);
   const [loading, setLoading] = useState(false);
+  const [paymentRequest, setPaymentRequest] = useState(null);
   const { setAlert }: any = useExpensesData();
   const stripe: any = useStripe();
   const elements = useElements();
@@ -35,6 +35,32 @@ const Payments: FC = (): JSX.Element => {
     formState: { errors },
   } = useForm<FormData>();
 
+  useEffect(() => {
+    if (stripe) {
+      const pr = stripe.paymentRequest({
+        country: "US",
+        currency: "usd",
+        total: {
+          label: "Demo total",
+          amount: 1099,
+        },
+        disableWallets: ["link"],
+        requestPayerName: true,
+        // requestPayerName: true,
+        // requestPayerEmail: true,
+      });
+
+      pr.canMakePayment()
+        .then((result: any) => {
+          if (result) {
+            console.log(result);
+            setPaymentRequest(pr);
+          }
+        })
+        .catch(console.error);
+    }
+  }, [stripe]);
+
   const intentRequst = (): void => {
     fetch("http://localhost:5001/intentd", {
       method: "POST",
@@ -44,7 +70,8 @@ const Payments: FC = (): JSX.Element => {
       .then((r) => r.json())
       .then((response) => {
         paymentRequst(response?.client_secret?.id);
-      });
+      })
+      .catch((e) => console.log(e));
   };
   const paymentRequst = async (id: string): Promise<void> => {
     try {
@@ -72,8 +99,8 @@ const Payments: FC = (): JSX.Element => {
           setAlert("error");
           setLoading(false);
         });
-    } catch {
-      console.log("error");
+    } catch (e) {
+      console.log(e, "error");
     }
   };
   const paymentConfirm = (updatedPaymentIntent: any) => {
@@ -97,105 +124,118 @@ const Payments: FC = (): JSX.Element => {
   };
 
   return (
-    <div className="payments">
-      <form className="payments-form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="payments-method">
-          <div className="payments-method__title">
-            <h3>Payment Method</h3>
-            <p>Enter your card information to make the payment securely.</p>
-            <CreditCard register={register} errors={errors} control={control} />
-          </div>
-        </div>
-        <div className="payments-address">
-          <div className="payments-address-lefth">
-            <input type="checkbox" {...register("chekbox", { required: true })} />
-          </div>
-          <div className="payments-address-rigth">
-            <p>
-              By checking this box, I acknowledge that I have read and agree to the <span>Terms of Service</span>, and{" "}
-              <span>Monthly Billing Terms</span> of this website and want to opt-in for the monthly billed Dream Collection Club®
-            </p>
-          </div>
-        </div>
-        <div className="payments-billing">
-          <div className="payments-billing__title">
-            <h3>Billing address</h3>
-            <p>Select the address that matches your card or payment method.</p>
-          </div>
-          <div className="payments-billing-topaddress">
-            <input {...register("radiotop")} type="radio" name="1" id="top" onClick={openOption} />
-            <label>Same as shipping address</label>
-          </div>
+    <>
+      <div className="payments">
+        {paymentRequest ? (
+          <PaymentRequestButtonElement options={{ paymentRequest }} />
+        ) : (
+          <form className="payments-form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="payments-method">
+              <div className="payments-method__title">
+                <h3>Payment Method</h3>
+                <p>Enter your card information to make the payment securely.</p>
+                <CreditCard register={register} errors={errors} control={control} />
+              </div>
+            </div>
+            <div className="payments-address">
+              <div className="payments-address-lefth">
+                <input type="checkbox" {...register("chekbox", { required: true })} />
+              </div>
+              <div className="payments-address-rigth">
+                <p>
+                  By checking this box, I acknowledge that I have read and agree to the <span>Terms of Service</span>, and{" "}
+                  <span>Monthly Billing Terms</span> of this website and want to opt-in for the monthly billed Dream Collection
+                  Club®
+                </p>
+              </div>
+            </div>
+            <div className="payments-billing">
+              <div className="payments-billing__title">
+                <h3>Billing address</h3>
+                <p>Select the address that matches your card or payment method.</p>
+              </div>
+              <div className="payments-billing-topaddress">
+                <input {...register("radiotop")} type="radio" name="1" id="top" onClick={openOption} />
+                <label>Same as shipping address</label>
+              </div>
 
-          <>
-            <div className={option ? "payments-billing-bottomaddress bottomaddress--mod" : "payments-billing-bottomaddress"}>
-              <input {...register("radiobottom")} type="radio" name="1" id="bottom" onClick={openOption} />
-              <label>Use a different billing address</label>
+              <>
+                <div className={option ? "payments-billing-bottomaddress bottomaddress--mod" : "payments-billing-bottomaddress"}>
+                  <input {...register("radiobottom")} type="radio" name="1" id="bottom" onClick={openOption} />
+                  <label>Use a different billing address</label>
+                </div>
+                <div className={option ? "bottomaddress__option option--mod" : "bottomaddress__option"}>
+                  <div className="bottomaddress__option-country">
+                    <img src={Vector} alt="vector" />
+                    <Form.Select {...register("country")} defaultValue="">
+                      <option value="" disabled>
+                        Select a Country
+                      </option>
+                      {countries.map(({ label }, i) => (
+                        <option key={i} value={label}>
+                          {label}
+                        </option>
+                      ))}
+                    </Form.Select>
+                    {errors?.country && <p className="error__text">Enter a valid country address</p>}
+                  </div>
+                  <div className="bottomaddress__option-firstlast">
+                    <div className="bottomaddress__option-firstname">
+                      <img src={User} alt="User" />
+                      <Input type="text" placeholder="First Name" register={register} name="firstname" errors={errors} />
+                    </div>
+                    <div className="bottomaddress__option-lastname">
+                      <img src={User} alt="User" />
+                      <Input type="text" placeholder="First Name" register={register} name="lastname" errors={errors} />
+                    </div>
+                  </div>
+                  <div className="bottomaddress__option-address">
+                    <img src={Address} alt="address" />
+                    <Input type="text" placeholder="Address" register={register} name="address" errors={errors} />
+                  </div>
+                  <div className="bottomaddress__option-apartament">
+                    <img src={Org} alt="org" />
+                    <Input
+                      type="text"
+                      placeholder="Aparment, suit, etc (Optional)"
+                      register={register}
+                      name="org"
+                      errors={errors}
+                    />
+                  </div>
+                  <div className="bottomaddress__option-citycode">
+                    <div className="bottomaddress__option-city">
+                      <Form.Select aria-label="Default select example" {...register("city")} defaultValue="">
+                        <option value="" disabled>
+                          City
+                        </option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </Form.Select>
+                    </div>
+                    <div className="bottomaddress__option-code">
+                      <Input type="text" placeholder="Zip Code" register={register} name="code" errors={errors} />
+                    </div>
+                  </div>
+                  <div className="bottomaddress__option-phone">
+                    <img src={Phone} alt="phone" />
+                    <Input
+                      type="text"
+                      placeholder="Phone Number ( for shipping updates & orders )"
+                      register={register}
+                      name="phone"
+                      errors={errors}
+                    />
+                  </div>
+                </div>
+              </>
             </div>
-            <div className={option ? "bottomaddress__option option--mod" : "bottomaddress__option"}>
-              <div className="bottomaddress__option-country">
-                <img src={Vector} alt="vector" />
-                <Form.Select {...register("country")} defaultValue="">
-                  <option value="" disabled>
-                    Select a Country
-                  </option>
-                  {countries.map(({ label }, i) => (
-                    <option key={i} value={label}>
-                      {label}
-                    </option>
-                  ))}
-                </Form.Select>
-                {errors?.country && <p className="error__text">Enter a valid country address</p>}
-              </div>
-              <div className="bottomaddress__option-firstlast">
-                <div className="bottomaddress__option-firstname">
-                  <img src={User} alt="User" />
-                  <Input type="text" placeholder="First Name" register={register} name="firstname" errors={errors} />
-                </div>
-                <div className="bottomaddress__option-lastname">
-                  <img src={User} alt="User" />
-                  <Input type="text" placeholder="First Name" register={register} name="lastname" errors={errors} />
-                </div>
-              </div>
-              <div className="bottomaddress__option-address">
-                <img src={Address} alt="address" />
-                <Input type="text" placeholder="Address" register={register} name="address" errors={errors} />
-              </div>
-              <div className="bottomaddress__option-apartament">
-                <img src={Org} alt="org" />
-                <Input type="text" placeholder="Aparment, suit, etc (Optional)" register={register} name="org" errors={errors} />
-              </div>
-              <div className="bottomaddress__option-citycode">
-                <div className="bottomaddress__option-city">
-                  <Form.Select aria-label="Default select example" {...register("city")} defaultValue="">
-                    <option value="" disabled>
-                      City
-                    </option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                  </Form.Select>
-                </div>
-                <div className="bottomaddress__option-code">
-                  <Input type="text" placeholder="Zip Code" register={register} name="code" errors={errors} />
-                </div>
-              </div>
-              <div className="bottomaddress__option-phone">
-                <img src={Phone} alt="phone" />
-                <Input
-                  type="text"
-                  placeholder="Phone Number ( for shipping updates & orders )"
-                  register={register}
-                  name="phone"
-                  errors={errors}
-                />
-              </div>
-            </div>
-          </>
-        </div>
-        <Buttons type="submit" imgs name="Validate the order" load={loading} />
-      </form>
-    </div>
+            <Buttons type="submit" imgs name="Validate the order" load={loading} />
+          </form>
+        )}
+      </div>
+    </>
   );
 };
 
